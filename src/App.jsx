ea48f5b13f51014fe8ec32db5a6eb3bb85f9d1bc
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-28 20:20:08 -0400

    add advanced styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { useState, useEffect, useRef } from 'react'
+import scanlines from './assets/scanlines.png'
+import bezel from './assets/bezel.png'
 
 function App() {
   const [input, setInput] = useState('');
@@ -7,44 +9,50 @@ function App() {
   const inputRef = useRef();
 
   useEffect(() => {
-    inputRef.current.focus();
+    inputRef.current.focus();    
   }, [])
 
   return (
-    <div 
-      className="App"
-      onClick={e => {inputRef.current.focus()}}
-    >
-      <input 
-        ref={inputRef}
-        type="text" 
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        onKeyDown={
-          e => {
-            if (e.key === "Enter") {
-              let newOutput = '';
-              newOutput = output + "\n" + "\n" + "$" + input + "\n";
-              switch (input) {
-                case "ls":
-                  newOutput += "List of projects";
-                  break;
-                case "pwd":
-                  newOutput += "You're on my cool terminal site";
-                  break;
-                default:
-                  newOutput += "Unknown Command"
+    <div id="screen">
+      <img src={scanlines} id="scanlines" class="noselect" />
+      <img src={bezel} id="bezel" class="noselect" />
+      <div 
+        className="App"
+        onClick={e => {inputRef.current.focus()}}
+      >
+        <input 
+          ref={inputRef}
+          type="text" 
+          placeholder="Type a command and press Enter"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          onKeyDown={
+            e => {
+              if (e.key === "Enter") {
+                let newOutput = '';
+                newOutput = output + "\n" + "\n" + "$" + input + "\n";
+                switch (input) {
+                  case "ls":
+                    newOutput += "List of projects";
+                    break;
+                  case "pwd":
+                    newOutput += "You're on my cool terminal site";
+                    break;
+                  default:
+                    newOutput += "Unknown Command"
+                }
+                setOutput(newOutput);
+                setInput('');
               }
-              setOutput(newOutput);
-              setInput('');
             }
           }
-        }
-      />
-      <div className="terminal">
-        {output}
+        />
+        <div className="terminal">
+          {output}
+        </div>
       </div>
     </div>
+
   )
 }
 
